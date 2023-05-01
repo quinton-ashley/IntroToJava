c5369b161fa53f7841eb71c5302cfe036781e2e7
Quinton Ashley <qashto@gmail.com>
2023-05-01 15:10:43 -0500

    2.0.88

--- Level_09/TicTacToe/TicTacToe.java
+++ Level_09/TicTacToe/TicTacToe.java
@@ -3,49 +3,49 @@ package games_java.TicTacToe;
 import static games_java.QuintOS.*;
 
 public class TicTacToe {
-  public static void main(String[] args) {
+  String title = """
+      TTTTT IIIII   CCC
+        T     I    C
+        T     I    C
+        T     I    C
+        T   IIIII   CCC
 
-    String title = """
-        TTTTT IIIII   CCC
-          T     I    C
-          T     I    C
-          T     I    C
-          T   IIIII   CCC
+      TTTTT  AAA    CCC
+        T   A   A  C
+        T   AAAAA  C
+        T   A   A  C
+        T   A   A   CCC
 
-        TTTTT  AAA    CCC
-          T   A   A  C
-          T   AAAAA  C
-          T   A   A  C
-          T   A   A   CCC
+      TTTTT  OOO   EEEE
+        T   O   O  E
+        T   O   O  EEE
+        T   O   O  E
+        T    OOO   EEEE""";
 
-        TTTTT  OOO   EEEE
-          T   O   O  E
-          T   O   O  EEE
-          T   O   O  E
-          T    OOO   EEEE""";
+  String bigO = " OOOOOO \n" +
+      "OO    OO\n" +
+      "OO    OO\n" +
+      "OO    OO\n" +
+      "OO    OO\n" +
+      "OO    OO\n" +
+      " OOOOOO ";
 
-    String bigO = " OOOOOO \n" +
-        "OO    OO\n" +
-        "OO    OO\n" +
-        "OO    OO\n" +
-        "OO    OO\n" +
-        "OO    OO\n" +
-        " OOOOOO ";
+  String bigX = "XX    XX\n" +
+      " XX  XX \n" +
+      "  XXXX  \n" +
+      "   XX   \n" +
+      "  XXXX  \n" +
+      " XX  XX \n" +
+      "XX    XX";
 
-    String bigX = "XX    XX\n" +
-        " XX  XX \n" +
-        "  XXXX  \n" +
-        "   XX   \n" +
-        "  XXXX  \n" +
-        " XX  XX \n" +
-        "XX    XX";
+  String bigSpace = "        \n".repeat(7);
 
-    String bigSpace = "        \n".repeat(7);
+  int gridRow = 3;
+  int gridCol = 26;
 
-    int gridRow = 3;
-    int gridCol = 26;
-
+  public void startGame() {
     txt(title, 5, 3);
+
     /* Part A: finish the grid of 9x8 spaces */
     txt("─".repeat(26), gridRow + 7, gridCol);
     // txt("─".repeat(26), ?, ?); // draw another horizontal line
@@ -53,12 +53,19 @@ public class TicTacToe {
     txt("│\n".repeat(23), gridRow, gridCol + 8);
     // txt("│\n".repeat(23), ?, ?); // draw another vertical line
 
-    /* Part A: Make the buttons in the grid */
-    // note the intervals! row += 8 and col += 9
-    // test code, delete after reading
+    // the following buttons were created just to help you make the grid
+    // you can delete them once you're done
     button(bigO, gridRow, gridCol);
     button(bigX, gridRow + 8, gridCol + 9);
     button(bigO, gridRow + 16, gridCol + 18);
+  }
+
+  public TicTacToe() {
+    startGame();
+  }
+
+  public static void main(String[] args) {
+    new TicTacToe();
   }
 
 }
